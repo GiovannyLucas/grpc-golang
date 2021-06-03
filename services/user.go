377981f98c741f6c943babcf8cee8e5c14075790
GiovannyLucas <giovannylucax@gmail.com>
2021-06-03 12:48:59 -0300

    feat: :sparkles: adding a fake datastream to test long requests

--- services/user.go
+++ services/user.go
@@ -2,12 +2,14 @@ package services
 
 import (
 	"context"
+	"time"
 
 	"github.com/GiovannyLucas/grpc-golang/pb"
 )
 
 // type UserServiceServer interface {
 // 	AddUser(context.Context, *User) (*User, error)
+// 	AddUserVerbose(ctx context.Context, in *User, opts ...grpc.CallOption) (UserService_AddUserVerboseClient, error)
 // 	mustEmbedUnimplementedUserServiceServer()
 // }
 
@@ -27,4 +29,42 @@ func (*UserService) AddUser(ctx context.Context, payload *pb.User) (*pb.User, er
 		Name:  payload.GetName(),
 		Email: payload.GetEmail(),
 	}, nil
+}
+
+func (*UserService) AddUserVerbose(payload *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+	stream.Send(&pb.UserResultStream{
+		Status: "Initing...",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Inserting on database...",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "User has been inserted!",
+		User: &pb.User{
+			Id:    "123",
+			Name:  payload.GetName(),
+			Email: payload.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Completed",
+		User: &pb.User{
+			Id:    "123",
+			Name:  payload.GetName(),
+			Email: payload.GetEmail(),
+		},
+	})
+
+	return nil
 }
